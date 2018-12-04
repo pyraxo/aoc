004b2be177424304acb4abe129f6f69929d7170e
pyraxo <pyraxo@tfwno.gf>
2018-12-04 13:58:37 +0800

    feat: day 2 part 2

--- 02/run.go
+++ 02/run.go
@@ -5,34 +5,39 @@ import (
 	"bufio"
 	"strings"
 	"time"
+	"log"
 	"os"
 )
 
 func check (e error) {
 	if e != nil {
-		fmt.Println(e)
+		log.Fatal(e)
 	}
 }
 
-var (
-	two int
-	three int
-	isTwo bool
-	isThree bool
-	total int
-)
-
 
 func main () {
-	// part 1
-	start := time.Now()
 	file, err := os.Open("./input.txt")
 	check(err)
 	defer file.Close()
 
+	// part 1
+	start := time.Now()
+
+	var (
+		two int
+		three int
+		isTwo bool
+		isThree bool
+		total int
+	)
+
+	var store []string
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
+		store = append(store, line)
 		for _, char := range line {
 			num := strings.Count(line, string(char))
 			if num >= 2 {
@@ -58,6 +63,35 @@ func main () {
 	}
 
 	total = two * three
-	fmt.Println(total)
-	fmt.Printf("--- %v ---\n", time.Since(start))
+	fmt.Printf("part 1: %v\n", total)
+	fmt.Printf("--- %v ---\n\n", time.Since(start))
+
+	// part 2
+	start = time.Now()
+	for i, line := range store {
+		storeLen := len(store)
+		for j := i + 1; j < storeLen; j++ {
+			var count int
+			var str string
+			compStr := store[j]
+			compLen := len(compStr)
+
+			for k := 0; k < compLen; k++ {
+				if line[k] == compStr[k] {
+					str += string(line[k])
+				} else {
+					count++
+				}
+				if count == 2 {
+					break
+				}
+			}
+			
+			if count < 2 {
+				fmt.Printf("part 2: %v\n", str)
+				fmt.Printf("--- %v ---\n", time.Since(start))
+				os.Exit(0)
+			}
+		}
+	}
 }
